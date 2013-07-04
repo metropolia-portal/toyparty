3bccd414474bae5c18212f8ca93f8015ef1ca4e4
Maxim Zavadskiy <Maxim.Zavadskiy@metropolia.fi>
2013-07-04 17:45:16 +0300

    Candy started

--- Assets/Scripts/CandyWizard/Candy.cs
+++ Assets/Scripts/CandyWizard/Candy.cs
@@ -5,6 +5,7 @@ public class Candy : MonoBehaviour {
 	//drops the candy
 	public void Drop() {
 		rigidbody.isKinematic = false;
+		rigidbody.useGravity = true
 	}
 	
 	// Use this for initialization
