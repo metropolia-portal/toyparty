5c6d79441f213d7270f172c1132e92bf5b7184af
Maxim Zavadskiy <Maxim.Zavadskiy@metropolia.fi>
2013-07-12 15:20:34 +0300

    Automatic layouting of main menu for different screen resolution, clean up the code. Import settings of some GUI textures are set to GUI.

--- Assets/Scripts/MainMenu/MainMenuGUI.cs
+++ Assets/Scripts/MainMenu/MainMenuGUI.cs
@@ -11,129 +11,84 @@ public class MainMenuGUI : MonoBehaviour {
 	public static int currentLevel = 1;
 	public static string selectedGameName;
 	
-	float ScreenHeight = Screen.height;
-	float ScreenWidth = Screen.width;
-	float ButtonWidth;
+	public float barHeightToScreenHeightRatio = 0.25f;
+	public float gamePreviewWidthToScreenWidthRatio = 0.75f;
+	public float gamePreviewArrowHeightRation = 0.2f;// height ration of the white speach arrow pointing to character to total height of preview screen
+	
+	public float hoverButtonSizeIncrease = 1.2f;
+	
+	//float ScreenHeight = Screen.height;
+	//float ScreenWidth = Screen.width;
+	
+	float buttonWidth;
+	float buttonBarHeight;
+	
 	float Margin = Screen.width/45;
-	float aspect = 0.0f;
 		
-	int NumberOfButtons;	
+	int gamesNumber;	
 
-	Texture PlayButton;
 	public string[] gameList;
 	Texture[] menuOptionTextures;
 	Texture[] previewTextures;
+	Texture[] playButtons;
+		
+	Rect gamePreviewButtonRect;
+	Rect gamePreviewRect;	
+	Rect[] rect;
+	Rect quitRect;
+	Rect creditsRect;
+	Rect gameTitleRect;
 	
 	GUIStyle NoStyle = new GUIStyle();
-	
-	Rect gamePreviewButton;
-	
-	Rect [] rect;
-	Rect [] rectBig;
-	Rect [] rectSmall;
-	
-	Rect [] menuRect = 	new Rect[2];
-	Rect [] menuRectBig = new Rect[2];
-	Rect [] menuRectSmall = new Rect[2];
-	float diffRect;
-	float width;
-	
+		
 	int selectedGame = -1;
 	
 	// Use this for initialization
 	void Start () {
 		
 		currentLevel = 1;
-		NumberOfButtons = gameList.Length;
-		aspect = (float)Screen.width / Screen.height;
+		gamesNumber = gameList.Length;
+		buttonBarHeight = barHeightToScreenHeightRatio * Screen.height;
 		
-		menuOptionTextures = new Texture[NumberOfButtons];
-		previewTextures = new Texture[NumberOfButtons];
-		rect = new Rect[NumberOfButtons];
-		rectBig = new Rect[NumberOfButtons];
-		rectSmall = new Rect[NumberOfButtons];
-		
-	#if UNITY_IPONE
-			gamePreviewButton = new Rect(Screen.width/2 - Screen.width/18, Screen.height/12 + Screen.height/2, Screen.width/7, Screen.width/7);
-	#else	
-		
-		if(aspect >= 1.23f && aspect <= 1.25f)// 5/4 aspect ratio
-		{ 
-			gamePreviewButton = new Rect(Screen.width/2 - Screen.width/14, Screen.height/14 + Screen.height/2, Screen.width/7, Screen.width/7);		
-		}
-		else if(aspect >= 1.32f && aspect <= 1.34f)// 4/3 aspect ratio
-		{ 
-			gamePreviewButton = new Rect(Screen.width/2 - Screen.width/14, Screen.height/12 + Screen.height/2, Screen.width/7, Screen.width/7);
-				
-		}
-		else if(aspect >= 1.49f && aspect <= 1.50f)// 3/2 aspect ratio
-		{ 
-			gamePreviewButton = new Rect(Screen.width/2 - Screen.width/14, Screen.height/24 + Screen.height/2, Screen.width/7, Screen.width/7);		
-				
-		}
-		else if(aspect >= 1.6f && aspect <= 1.61f)// web play aspect ratio
-		{ 
-			gamePreviewButton = new Rect(Screen.width/2 - Screen.width/18, Screen.height/12 + Screen.height/2, Screen.width/7, Screen.width/7);		
-				
-		}
-		else if(aspect >= 1.77f && aspect <= 1.78f)// 16/9 aspect ratio
-		{ 
-			gamePreviewButton = new Rect(Screen.width/2 - Screen.width/12, Screen.height/6 + Screen.height/3, Screen.width/7, Screen.width/7);		
-				
-		}
-		else// all other ratios
-		{	
-			gamePreviewButton = new Rect(Screen.width/2 - Screen.width/18, Screen.height/12 + Screen.height/2, Screen.width/7, Screen.width/7);	
-		}
-	#endif
-	
-		ButtonWidth = ScreenWidth/(NumberOfButtons-0.09f);
-		
-		for (int i = 0; i < NumberOfButtons; i++) {
+		menuOptionTextures = new Texture[gamesNumber];
+		previewTextures = new Texture[gamesNumber];
+		playButtons = new Texture[gamesNumber];
+		rect = new Rect[gamesNumber];
 
-			if(aspect >= 1.23f && aspect <= 1.25f) // 5/4 aspect ratio
-			{
-				rect[i] = new Rect(i * ButtonWidth, ScreenHeight/30, ButtonWidth , ButtonWidth);	
-			}
-			else if(aspect >= 1.32f && aspect <= 1.34f)// 4/3 aspect ratio
-			{ 
-				rect[i] = new Rect(i * (ButtonWidth) + Margin, ScreenHeight/25, ButtonWidth - Margin*2, ButtonWidth - Margin*2);
-			}
-			else if(aspect >= 1.49f && aspect <= 1.50f)// 3/2 aspect ratio
-			{ 
-				rect[i] = new Rect(i * (ButtonWidth) + Margin, ScreenHeight/40, ButtonWidth - Margin*2, ButtonWidth - Margin*2);		
-			}
-			else// all other ratios
-			{
-				rect[i] = new Rect(i * (ButtonWidth) + Margin, ScreenHeight/55, ButtonWidth - Margin*2, ButtonWidth - Margin*2);		
-			}
-	
-			menuOptionTextures[i] = (Texture)Resources.Load("MainMenu/Buttons/"+gameList[i]);
-			previewTextures[i] = (Texture)Resources.Load("MainMenu/Previews/"+gameList[i]);
-		}
-		for (int i = 0; i < NumberOfButtons; i++) 
-		{
-			rectSmall[i] = rect[i];
-		}
 
-		float widthNormal = rect[0].width;
-		float widthBig = widthNormal * 1.2f;
-		float diffWidth = (widthBig - widthNormal) / 2f;
-		for (int i = 0; i < NumberOfButtons; i++) 
-		{
-			rectBig[i] = rect[i];
-			rectBig[i].height = rectBig[i].width = widthBig;
-			rectBig[i].x = rectBig[i].x - diffWidth;
-			rectBig[i].y = rectBig[i].y - diffWidth ;
+		buttonWidth = (Screen.width - Margin*(gamesNumber+1)) /(gamesNumber);
+		
+		for (int i = 0; i < gamesNumber; i++) {
+			rect[i] = new Rect(i * (buttonWidth) + (i+1) * Margin, centerPosition(buttonWidth, buttonBarHeight ), buttonWidth, buttonWidth);
+			
+			menuOptionTextures[i] = (Texture)Resources.Load("MainMenu/Buttons/" + gameList[i]);
+			previewTextures[i] = (Texture)Resources.Load("MainMenu/Previews/" + gameList[i]);
+			playButtons[i] = (Texture)Resources.Load("MenuCommon/play_" + gameList[i]);
 		}
 		
-		menuRect[0] = GetMenuRect(1);
-		menuRectSmall[0] = new Rect(menuRect[0]);
-		menuRectBig[0] = new Rect(menuRect[0].x - diffWidth, menuRect[0].y - diffWidth,widthBig ,widthBig);
+		float titleHeight = Screen.height - buttonBarHeight - Margin;
+		float titleWidth = titleHeight * gameTitleTexture.width / gameTitleTexture.width;
+		gameTitleRect = new Rect(centerPosition(titleWidth, Screen.width), Screen.height - titleHeight, titleWidth, titleHeight);
 		
-		menuRect[1] = GetMenuRect(2);
-		menuRectSmall[1] = new Rect(menuRect[1]);
-		menuRectBig[1] = new Rect(menuRect[1].x - diffWidth, menuRect[1].y - diffWidth,widthBig ,widthBig);
+		quitRect = new Rect(Margin, Screen.height - Margin - buttonWidth, buttonWidth, buttonWidth);
+		
+		creditsRect = new Rect(Screen.width - Margin - buttonWidth, Screen.height - Margin - buttonWidth, buttonWidth, buttonWidth);
+					
+		float previewRatio = (float) previewTextures[0].width / previewTextures[0].height;	
+		float previewWidth = Screen.width * gamePreviewWidthToScreenWidthRatio;
+		float previewHeight = Mathf.Min ( previewWidth / previewRatio, Screen.height - buttonBarHeight - 2 * Margin);	
+		gamePreviewRect = new Rect(centerPosition(previewWidth, Screen.width), buttonBarHeight + Margin , previewWidth, previewHeight);
+		//TODO make centerRect method
+		gamePreviewButtonRect = centerInRect(new Rect(0, gamePreviewArrowHeightRation * gamePreviewRect.height, buttonWidth, buttonWidth), gamePreviewRect);// new Rect(centerPosition(), centerPosition(), Screen.width/7, Screen.width/7);			
+	}
+	
+	//inner.x and y are used for offset
+	static Rect centerInRect(Rect inner, Rect outer) {
+		return new Rect(outer.x + centerPosition(inner.width, outer.width)+ inner.x, outer.y + centerPosition(inner.height, outer.height) +  inner.y,inner.width, inner.height);
+	}
+	
+	static float centerPosition(float itemLength, float totalLength) {
+		return (totalLength - itemLength)/2;
 	}
 	
 	void SelectGame(int i) {
@@ -142,152 +97,52 @@ public class MainMenuGUI : MonoBehaviour {
 	}
 	
 	void OnGUI() {
-		var mousePos = InputManager.MouseScreenToGUI(); 
-		for(int i=0; i< NumberOfButtons; i++) 
-		{		
-			if(rect[i].Contains(mousePos))
-			{
-				rect[i] = rectBig[i];
-
-			}else{
-				rect[i] = rectSmall[i] ;
-			}
-		}
-		
-		for (int i=0; i< NumberOfButtons; i++) 
+		for (int i=0; i< gamesNumber; i++) 
 		{
-			if (GUI.Button(rect[i], menuOptionTextures[i], NoStyle)) 
+			if (ButtonWithHover(rect[i], menuOptionTextures[i])) 
 			{
 				SelectGame(i);
 			}
 		}
+		
 		if (selectedGame > -1 ) {
-			
-			if(aspect >= 1.77 && aspect <= 1.78){// 16/9 aspect ratio
+
+			GUI.DrawTexture(gamePreviewRect,  previewTextures[selectedGame]);		
 				
-				GUI.DrawTexture(new Rect(Screen.width/2 - (Screen.width/90 + Screen.width/3) , Screen.height/4 + Screen.width/45, Screen.width - Screen.width/3 , Screen.width/3 ), previewTextures[selectedGame]);	 
-			}
-			else if(aspect >= 1.49f && aspect <= 1.50f)// 3/2 aspect ratio
-			{ 
-				GUI.DrawTexture(new Rect(Screen.width/2 - (Screen.width/90 + Screen.width/3) , Screen.height/4 + Screen.width/70, Screen.width - Screen.width/3 , Screen.height/2 + Screen.height/10), previewTextures[selectedGame]);	 
-					
-			}
-			
-			else
-				GUI.DrawTexture(new Rect(Screen.width/2 - (Screen.width/80 + Screen.width/3) , Screen.height/4 + Screen.width/45, Screen.width - Screen.width/3 , Screen.width/2 ), previewTextures[selectedGame]);	
-			PlayButton = (Texture)Resources.Load("MenuCommon/play_" + selectedGameName);
-			
-			if (GUI.Button(gamePreviewButton, PlayButton, NoStyle)) 
+			if (ButtonWithHover(gamePreviewButtonRect, playButtons[selectedGame])) 
 			{
 				Application.LoadLevel("TutorialScene");
-				//Camera.DontDestroyOnLoad(Camera.main.audio);
 			}
 		}
 		else
 		{
-			float ButtonArea = Screen.height/4;
-		#if UNITY_IPONE
-			GUI.DrawTexture(new Rect(Screen.width/3 -Screen.width/16 , Screen.height/4 + Margin*2, Screen.width/2, Screen.height/2 + Screen.height/6 ), gameTitleTexture);
-			
-		#else
-			
-			if(aspect >= 1.77 && aspect <= 1.78){// 16/9 aspect ratio
-				
-				GUI.DrawTexture(new Rect(Screen.width/3 -Screen.width/16 , Screen.height/5 + Margin*4, Screen.width/2, Screen.height/2 + Screen.height/6 ), gameTitleTexture);
-			}
-			
-			else if(aspect >= 1.23f && aspect <= 1.25f){// 5/4 aspect ratio
-				
-				GUI.DrawTexture(new Rect(Screen.width/3 -Screen.width/16 , Screen.height/6 + Margin*7, Screen.width/2, Screen.height/2 + Screen.height/6 ), gameTitleTexture); 
-			} 
-			else if(aspect >= 1.32f && aspect <= 1.34f){// 4/3 aspect ratio
-				
-				GUI.DrawTexture(new Rect(Screen.width/3 -Screen.width/16 , Screen.height/6 + Margin*6, Screen.width/2, Screen.height/2 + Screen.height/6 ), gameTitleTexture); 
-			}
-			else if(aspect >= 1.49f && aspect <= 1.50f){// 3/2 aspect ratio
-				
-				GUI.DrawTexture(new Rect(Screen.width/3 -Screen.width/16 , Screen.height/7 + Margin*6, Screen.width/2, Screen.height/2 + Screen.height/6 ), gameTitleTexture); 
-			}
-				
-			else {
-			
-				GUI.DrawTexture(new Rect(Screen.width/3 -Screen.width/16 , Screen.height/5 + Margin*4, Screen.width/2, Screen.height/2 + Screen.height/6 ), gameTitleTexture); 
-			}
-		#endif
+			GUI.DrawTexture(gameTitleRect, gameTitleTexture); 
 		}	
-		/*if (GUI.Button(new Rect(Margin,Screen.height - (Screen.width/6), Screen.width/7, Screen.width/7), gameExitTexture, NoStyle)) {
+		
+		if (ButtonWithHover( quitRect, gameExitTexture)) 
+		{
 			Application.Quit();
 		}
 		
-		if (GUI.Button(new Rect(Screen.width -Screen.width/6 + Margin/2, Screen.height - (Screen.width/6), Screen.width/7, Screen.width/7), gameCreditsTexture, NoStyle)) {
-			//Application.LoadLevel("CreditsSCreen");
-			//Camera.DontDestroyOnLoad(Camera.main.audio);
-		}*/
-		
-		// Drawing of the two bottom buttons
-		if(menuRect[0].Contains(mousePos))
-		{
-			menuRect[0] = menuRectBig[0];
-		}else
-		{
-			menuRect[0] = menuRectSmall[0];
-		}
-		if (GUI.Button( menuRect[0], gameExitTexture, NoStyle)) 
-		{
-			Application.Quit();
-		}
-
-		if(menuRect[1].Contains(mousePos))
-		{
-			menuRect[1] = menuRectBig[1];
-		}else
-		{
-			menuRect[1] = menuRectSmall[1];
-		}
-		if (GUI.Button(menuRect[1], gameCreditsTexture, NoStyle)) 
+		if (ButtonWithHover(creditsRect, gameCreditsTexture)) 
 		{
 			if(AudioListener.volume == 0){
 				AudioListener.volume = 1;
 			}else{
 				AudioListener.volume = 0;
-			} 
-				
+			} 		
 		}
 	}
-	Rect GetMenuRect(int num) {
+	
+	public bool ButtonWithHover(Rect pos, Texture image) {
+		Rect rect = pos;
+		if(pos.Contains(InputManager.MouseScreenToGUI())) {
+			float hoverW = buttonWidth * hoverButtonSizeIncrease;
+			float halfWidthDiff = (hoverW - buttonWidth) / 2f;
+			rect = new Rect(pos.x - pos.width * (hoverButtonSizeIncrease - 1) / 2f, pos.y - pos.height * (hoverButtonSizeIncrease - 1) / 2f, pos.width * hoverButtonSizeIncrease , pos.height * hoverButtonSizeIncrease);
+		}
 		
-		Rect menuRect, menuRect1;
-		
-		if(aspect >= 1.77 && aspect <= 1.78){// 16/9 aspect ratio
 			
-			menuRect = new Rect( Margin, Screen.height - (Screen.width/6 + Screen.height/30), Screen.width/6, Screen.width/6);
-			menuRect1 = new Rect(Screen.width - (Screen.width/5 + Margin/30), Screen.height - (Screen.width/6 + Screen.height/30), Screen.width/6, Screen.width/6);	
-		}
-			
-		else if(aspect >= 1.23f && aspect <= 1.25f){// 5/4 aspect ratio
-			
-			menuRect = new Rect( Margin, Screen.height - (Screen.width/6 + Screen.height/30), Screen.width/6, Screen.width/6);
-			menuRect1 = new Rect(Screen.width - (Screen.width/5 + Margin/30), Screen.height - (Screen.width/6 + Screen.height/30), Screen.width/6, Screen.width/6);		
-		} 
-		else if(aspect >= 1.32f && aspect <= 1.34f){// 4/3 aspect ratio
-			
-			menuRect = new Rect( Margin, Screen.height - (Screen.width/6 + Screen.height/30), Screen.width/6, Screen.width/6);
-			menuRect1 = new Rect(Screen.width - (Screen.width/5 + Margin/30), Screen.height - (Screen.width/6 + Screen.height/30), Screen.width/6, Screen.width/6);			 
-		}
-		else if(aspect >= 1.49f && aspect <= 1.50f){// 3/2 aspect ratio
-			
-			menuRect = new Rect( Margin, Screen.height - (Screen.width/6 + Screen.height/30), Screen.width/6, Screen.width/6);
-			menuRect1 = new Rect(Screen.width - (Screen.width/5 + Margin/30), Screen.height - (Screen.width/6 + Screen.height/30), Screen.width/6, Screen.width/6);			 
-		}
-				
-		else {
-			
-			menuRect = new Rect( Margin, Screen.height - (Screen.width/6 + Screen.height/30), Screen.width/6, Screen.width/6);
-			menuRect1 = new Rect(Screen.width - (Screen.width/5 + Margin/30), Screen.height - (Screen.width/6 + Screen.height/30), Screen.width/6, Screen.width/6);		 
-		}
-		 if(num == 1)
-		 return menuRect;
-		else
-		return menuRect1;
+		return GUI.Button(rect, image, NoStyle);
 	}
 }
