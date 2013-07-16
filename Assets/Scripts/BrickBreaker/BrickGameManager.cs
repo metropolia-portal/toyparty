5dbaea898bd6c8cfdb3e918a10abbf41ede5e2dd
Maxim Zavadskiy <Maxim.Zavadskiy@metropolia.fi>
2013-07-16 17:56:20 +0300

    Level 3 added. Fixed rotating marbles on game over. Destroying extra marble on all spheres lost

--- Assets/Scripts/BrickBreaker/BrickGameManager.cs
+++ Assets/Scripts/BrickBreaker/BrickGameManager.cs
@@ -231,6 +231,10 @@ public class BrickGameManager : GameManager
 		
 		SetMedal(medal);
 		
+		//stop all spheres exisiting
+		foreach(GameObject sphere in GameObject.FindGameObjectsWithTag("Sphere"))
+			sphere.GetComponent<Sphere>().setDirection(Vector2.zero);
+		
 		//do effects unless game is lost by loosing all spheres
 		if(spheres > 0) {
 			Invoke("DestroyAllPickups", 0.01f); //wait for the last pickup to be generated, then destroy all
@@ -243,8 +247,6 @@ public class BrickGameManager : GameManager
 	}
 	
 	void PlaySpheresFinishEffect() {
-		foreach(GameObject sphere in GameObject.FindGameObjectsWithTag("Sphere"))
-			sphere.GetComponent<Sphere>().setDirection(Vector2.zero);
 			
 		Invoke ("ExplodeSpheres", gameFinishedDelay - sphereExplosionTime );
 	}
