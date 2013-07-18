c5c68b70f873ae9243ed10aee24c273315dc6b61
Jason Dansie <jasondansie@gmail.com>
2013-07-18 11:02:51 +0300

    made option menu for sound and credit button

--- Assets/Scripts/MainMenu/MainMenuGUI.cs
+++ Assets/Scripts/MainMenu/MainMenuGUI.cs
@@ -19,15 +19,19 @@ public class MainMenuGUI : MonoBehaviour {
 	//float ScreenWidth = Screen.width;
 	
 	float buttonBarHeight;
-	int gamesNumber;	
+	int gamesNumber;
+	bool isSounON = true, callOptions = false;
 
 	public string[] gameList;
 	Texture[] gameSelectionTextures;
 	Texture[] previewTextures;
 	Texture[] playButtonsTextures;
+	Texture soundON, soundOff, credits, options;
 		
 	Rect gamePreviewButtonRect;
 	Rect gamePreviewRect;	
+	Rect soundButtonRect;
+	Rect creditsButtonRect;
 	Rect[] gameSelectionRects;
 	Rect quitRect;
 	Rect creditsRect;
@@ -54,6 +58,10 @@ public class MainMenuGUI : MonoBehaviour {
 			gameSelectionTextures[i] = (Texture)Resources.Load("MainMenu/Buttons/" + gameList[i]);
 			previewTextures[i] = (Texture)Resources.Load("MainMenu/Previews/" + gameList[i]);
 			playButtonsTextures[i] = (Texture)Resources.Load("MenuCommon/play_" + gameList[i]);
+			soundON =(Texture)Resources.Load("MainMenu/Buttons/soundon");
+			soundOff =(Texture)Resources.Load("MainMenu/Buttons/soundoff");
+			options =(Texture)Resources.Load("MainMenu/Buttons/brick_credits_btn");
+			credits =(Texture)Resources.Load("MainMenu/Buttons/roll_credits_btn");
 			
 			float buttonHeight = buttonWidth * gameSelectionTextures[i].height / gameSelectionTextures[i].width;
 			gameSelectionRects[i] = new Rect(i * (buttonWidth) + (i+1) * MGUI.Margin, centerPosition(buttonHeight, buttonBarHeight ), buttonWidth, buttonHeight);
@@ -71,6 +79,8 @@ public class MainMenuGUI : MonoBehaviour {
 		float previewWidth = Screen.width * gamePreviewWidthToScreenWidthRatio;
 		float previewHeight = Mathf.Min ( previewWidth / previewRatio, Screen.height - buttonBarHeight - 2 * MGUI.Margin);	
 		gamePreviewRect = new Rect(centerPosition(previewWidth, Screen.width), buttonBarHeight + MGUI.Margin , previewWidth, previewHeight);
+		soundButtonRect = MGUI.centerInRect(new Rect(80, gamePreviewArrowHeightRation * gamePreviewRect.height, MGUI.menuButtonWidth, MGUI.menuButtonWidth), gamePreviewRect);
+		creditsButtonRect = MGUI.centerInRect(new Rect(-70, gamePreviewArrowHeightRation * gamePreviewRect.height, MGUI.menuButtonWidth, MGUI.menuButtonWidth), gamePreviewRect);
 		//TODO make centerRect method
 		gamePreviewButtonRect = MGUI.centerInRect(new Rect(0, gamePreviewArrowHeightRation * gamePreviewRect.height, MGUI.menuButtonWidth, MGUI.menuButtonWidth), gamePreviewRect);// new Rect(centerPosition(), centerPosition(), Screen.width/7, Screen.width/7);			
 	}
@@ -89,11 +99,12 @@ public class MainMenuGUI : MonoBehaviour {
 		{
 			if (MGUI.HoveredButton(gameSelectionRects[i], gameSelectionTextures[i])) 
 			{
+				callOptions = false;
 				SelectGame(i);
 			}
 		}
 		
-		if (selectedGame > -1 ) {
+		if (selectedGame > -1 && callOptions == false) {
 
 			GUI.DrawTexture(gamePreviewRect,  previewTextures[selectedGame]);		
 				
@@ -102,24 +113,62 @@ public class MainMenuGUI : MonoBehaviour {
 				Application.LoadLevel("TutorialScene");
 			}
 		}
-		else
+		else if(callOptions == true)
 		{
-			GUI.DrawTexture(gameTitleRect, gameTitleTexture); 
-		}	
+			GUI.DrawTexture(gamePreviewRect,  previewTextures[1]);
+			if(isSounON){
+				
+				if (MGUI.HoveredButton(soundButtonRect, soundON)){
+					
+					EnableSound();
+				}
+			}
+			else{
+				
+				if (MGUI.HoveredButton(soundButtonRect, soundOff)){
+					
+					EnableSound();
+				}
+				
+			}
+			if (MGUI.HoveredButton(creditsButtonRect, credits)) {
+				
+				Application.LoadLevel("CreditsScreen");	
+			}
+			
+		}
+		else{
+			GUI.DrawTexture(gameTitleRect, gameTitleTexture);
+		}
 		
 		if (MGUI.HoveredButton( quitRect, gameExitTexture)) 
 		{
 			Application.Quit();
 		}
 		
-		if (MGUI.HoveredButton(creditsRect, gameCreditsTexture)) 
+		if (MGUI.HoveredButton(creditsRect, options)) 
 		{
-			if(AudioListener.volume == 0){
-				AudioListener.volume = 1;
-			}else{
-				AudioListener.volume = 0;
-			} 		
+			callOptions = true;
 		}
+	}
+	
+	void EnableSound(){
+		
+		if(AudioListener.volume == 0){
+			
+			AudioListener.volume = 1;
+		}
+			
+		else{
+				AudioListener.volume = 0;
+		} 
+		if(isSounON){
+				isSounON = false;	
+		}	
+		else{
+				isSounON = true;
+		}
+		
 	}
 	
 
