4ac12a1ae68055f22ac41a1def745c6be527512b
Maxim Zavadskiy <Maxim.Zavadskiy@metropolia.fi>
2013-07-24 18:33:54 +0300

    Flask sprites and button graphics

--- Assets/Scripts/CandyWizard/GameGUI.cs
+++ Assets/Scripts/CandyWizard/GameGUI.cs
@@ -5,9 +5,11 @@ public class GameGUI : MonoBehaviour {
 	
 	public bool enableSpeedup = true;
 	public Texture speedupSpriteSheet;
-	public int sprites = 6;
+	public int sprites = 7;
 	public float flaskHeightToScreenHeightRation = 0.1f;
-	public float flaskButtonHeighToTextureHeightRation = 0.1f;
+	
+	public float flaskXRelativePos = 0.8f; //relative to screen width
+	public float flaskYRelativePos = 0.2f; //relative to screen height
 	
 	bool speedupOn = false;
 	float speedupLeft = 1f;
@@ -22,16 +24,16 @@ public class GameGUI : MonoBehaviour {
 			//box dimensiont is 1/6 of button dimension
 			
 			float flaskHeight = Screen.height * flaskHeightToScreenHeightRation;
-			float flaskWidth = flaskHeight * (speedupSpriteSheet.width /(float) sprites) / speedupSpriteSheet.height; 
+			float flaskWidth = flaskHeight * (speedupSpriteSheet.width /(float) sprites) / speedupSpriteSheet.height;
+			
+			GUI.BeginGroup(new Rect( Screen.width * flaskXRelativePos, Screen.height * flaskYRelativePos,flaskWidth ,flaskHeight)); 
 			
 			//we should take size of the whole sprite, so that one frame dimension is our needed height and width
 			float buttonWidth = flaskWidth * sprites; 
 			
-			GUI.BeginGroup(new Rect(50,40,flaskWidth ,flaskHeight)); 
-			
 		//	first argument - offset of target frame
 			print(( (int)( (1 - speedupLeft) * sprites)));
-			if (GUI.Button(new Rect( - ( (int)( (1 - speedupLeft) * (sprites-1))) * flaskWidth, 0, buttonWidth,  buttonWidth), speedupSpriteSheet, MGUI.NoStyle)) {
+			if (GUI.Button(new Rect( - ( (int)( (1 - speedupLeft) * (sprites - 1))) * flaskWidth, 0, buttonWidth,  buttonWidth), speedupSpriteSheet, MGUI.NoStyle)) {
 				speedupOn = !speedupOn;
 				CandyWizardGameManager.Instance().SetSpeedUpBrushActive(speedupOn);
 			}
