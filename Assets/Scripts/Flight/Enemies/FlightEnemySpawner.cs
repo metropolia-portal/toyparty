658957311f1879f6ffb9da52e34d8646204cb0be
FRTT <rocket.coverall@gmail.com>
2013-08-14 17:34:58 +0300

    enemy spawners in flight

--- Assets/Scripts/Flight/Enemies/FlightEnemySpawner.cs
+++ Assets/Scripts/Flight/Enemies/FlightEnemySpawner.cs
@@ -2,14 +2,35 @@
 using System.Collections;
 
 public class FlightEnemySpawner : MonoBehaviour {
+	public GameObject enemy;	
+	public float startTime = 0;
+	public float spawnDelay = 1;
+	public float endTime = 1;
+	public float charges = 0;
+	public float spawnDelayRandom = 0;
+	public Bounds position;
+
+	
+	float timeLeft;
 
 	// Use this for initialization
 	void Start () {
-	
+		timeLeft = startTime;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+			timeLeft -= Time.deltaTime;
+			if (timeLeft <=0) {
+				timeLeft = spawnDelay + Random.Range(-spawnDelayRandom, +spawnDelayRandom);
+				Vector3 pos = new Vector3 (Random.Range(position.min.x,position.max.x), transform.position.y, Random.Range(position.min.z,position.max.z));
+				Instantiate(enemy, pos, transform.rotation);
+			}					
+			
+		if (endTime > 0) {
+			endTime -= Time.deltaTime;
+			if (endTime <=0) enabled = false;
+		}
 	}
 }
