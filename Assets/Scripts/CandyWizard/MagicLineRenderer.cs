6bef686e7535841e24bfc8029cfdc7be5c2778a9
Maxim Zavadskiy <Maxim.Zavadskiy@metropolia.fi>
2013-08-15 12:43:16 +0300

    Stars and Candy Graphics. Candy bar obstacles. Magic line candy bar texture

---
+++ Assets/Scripts/CandyWizard/MagicLineRenderer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicLineRenderer : MonoBehaviour {
+	public float tilesPerMeter = 2;
+	public float minSegmentLength = 0.001f;
+		
+	Vector3 prevPos;
+	float length = 0;
+	int vertices = 1;
+
+	
+	LineRenderer lineR;
+	// Use this for initialization
+	void Start () {
+		prevPos = transform.position;
+		lineR = GetComponent<LineRenderer>();
+		lineR.SetPosition(0, transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((transform.position - prevPos).magnitude > minSegmentLength) {
+			length += (transform.position - prevPos).magnitude;
+			prevPos = transform.position;
+			
+	
+			vertices++;
+			lineR.SetVertexCount(vertices);
+			lineR.SetPosition( vertices-1, transform.position);
+			
+			renderer.material.mainTextureScale= new Vector2(length*tilesPerMeter, 1);
+		}
+	}
+}
