32944163676602288c939eb13b640c81e0f9d2ad
Maxim Zavadskiy <Maxim.Zavadskiy@metropolia.fi>
2013-08-19 20:48:41 +0300

    More levels, autotiling of bars

---
+++ Assets/Scripts/CandyWizard/KeepTilingRatio.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode()]
+public class KeepTilingRatio : MonoBehaviour {
+	
+	public float tilesPerMeter = 5;
+	public Material material;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 targetScale = new Vector2(transform.localScale.x*tilesPerMeter, 1);
+		if(renderer.material.mainTextureScale != targetScale ) { //to block touching scene always
+			renderer.material = material;
+			renderer.material.mainTextureScale =  targetScale;
+		}
+	}
+}
