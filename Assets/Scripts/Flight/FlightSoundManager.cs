c2d266431e954d567fd2aa2df71e017c4edef7d9
FRTT <rocket.coverall@gmail.com>
2013-08-22 13:48:59 +0300

    squirrel art

---
+++ Assets/Scripts/Flight/FlightSoundManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlightSoundManager : MonoBehaviour {
+	
+	public AudioClip OwlDeath;
+	public AudioClip SquirrelDeath;
+	public AudioClip DragonDeath;
+	public AudioClip OwlBossDamage;
+	public AudioClip OwlBossDeath;
+	public AudioClip OwlBossShot;
+	public AudioClip Pickup;
+	public AudioClip AttackB;
+	public AudioClip AttackA;
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+		audioSource.loop = false;
+		audio.volume = 1;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void PlaySound(AudioClip sound) {
+		if (sound == null) return;
+		if (audioSource.isPlaying) return;
+		audioSource.clip = sound;
+		audioSource.Play();		
+	}
+	
+}
