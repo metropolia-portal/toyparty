89f7145883918b90183a9150ea64cb5beecb7b3a
Maxim Zavadskiy <Maxim.Zavadskiy@metropolia.fi>
2013-08-23 17:38:20 +0300

    Speedup fixed for box collider brush

--- Assets/Scripts/CandyWizard/SpeedUpSegment.cs
+++ Assets/Scripts/CandyWizard/SpeedUpSegment.cs
@@ -28,17 +28,20 @@ public class SpeedUpSegment : MonoBehaviour {
 		if(col.CompareTag("Candy")) {
 			//col.rigidbody.velocity *= ( 1 + 5f * Time.deltaTime);
 			//col.rigidbody.velocity += collision.frictionForceSum;
-//			Vector3 colliderDirection = Vector3.;
-//			
-//			
-////			Vector3 extraVelocity = (-vert[0] + vert[1]).normalized * col.rigidbody.velocity.magnitude * 2f * Time.deltaTime ; 
-////			Debug.DrawRay(col.transform.position,extraVelocity*5f,Color.red, 0.5f);
-////					
-////			//extraVelocity.y -= Mathf.Abs(extraVelocity.y)* 0.3f;
-////			col.rigidbody.velocity += extraVelocity;
-//			
-//			//projecting speed on the direction of the segment, candy will speedup in any direction
-//			col.rigidbody.velocity = Vector3.Project(col.rigidbody.velocity, vert[1] - vert[0]).normalized * (col.rigidbody.velocity.magnitude + acceleration * Time.deltaTime);
+			Vector3 colliderDirection =  transform.forward;
+			print(transform.TransformDirection(Vector3.right));
+			Debug.DrawRay(col.transform.position,colliderDirection*5f,Color.red, 0.5f);
+			//print(transform.rotation.eulerAngles.x);
+			//print(colliderDirection);
+			
+//			Vector3 extraVelocity = (-vert[0] + vert[1]).normalized * col.rigidbody.velocity.magnitude * 2f * Time.deltaTime ; 
+//			Debug.DrawRay(col.transform.position,extraVelocity*5f,Color.red, 0.5f);
+//					
+//			//extraVelocity.y -= Mathf.Abs(extraVelocity.y)* 0.3f;
+//			col.rigidbody.velocity += extraVelocity;
+			
+			//projecting speed on the direction of the segment, candy will speedup in any direction
+			col.rigidbody.velocity = Vector3.Project(col.rigidbody.velocity, colliderDirection).normalized * (col.rigidbody.velocity.magnitude + acceleration * Time.deltaTime);
 			
 			//col.rigidbody.AddTorque(Vector2.right * 5f * Time.deltaTime);
 			
